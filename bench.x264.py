7ec0d849c75cbea7f11a54f8cd1a2b96ef6e1c7f
nwgat <github@nwgat.net>
2017-02-14 14:52:57 +0100

    Create bench.x264.py

---
+++ bench.x264.py
@@ -0,0 +1,12 @@
+# The Fire Escape 4K Benchmark Beta 1
+# https://nwgat.ninja/TheFireEscape
+
+import time 
+import os
+import sys
+sys.stdout=open("output.x264.txt","a")
+cmd = 'qmelt.exe -progress -verbose TheFireEscape.mlt -consumer avformat:TheFireEscape.x264.mkv acodec=flac vcodec=libx264 preset=slow crf=16'
+start_time = time.time()
+os.system(cmd)
+print("4K x264 %s seconds" % (time.time() - start_time))
+sys.stdout.close()
