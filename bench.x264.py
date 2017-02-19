9f3605d59e62efecafb9d3552c82d9197fa2221a
nwgat <github@nwgat.net>
2017-02-19 15:47:47 +0100

    Update bench.x264.py

--- bench.x264.py
+++ bench.x264.py
@@ -4,7 +4,7 @@
 import time 
 import os
 import sys
-sys.stdout=open("output.txt","a")
+sys.stdout=open("output.x264.txt","a")
 cmd = 'Shotcut\qmelt.exe -progress -verbose TheFireEscape.mlt -consumer avformat:TheFireEscape.x264.mkv acodec=flac vcodec=libx264 preset=slow crf=16'
 start_time = time.time()
 os.system(cmd)
