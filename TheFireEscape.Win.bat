0c0d0ca85f6a0454c10ed60b20b2aca2b407679b
nwgat <github@nwgat.net>
2017-06-19 19:52:10 +0200

    Update TheFireEscape.Win.bat

--- TheFireEscape.Win.bat
+++ TheFireEscape.Win.bat
@@ -19,7 +19,6 @@ IF %M%==4 GOTO EOF
 :install
 wget -nc https://awesome.nwgat.ninja/TheFireEscape/TheFireEscape.raw.7z
 wget -nc https://github.com/mltframework/shotcut/releases/download/v17.02/shotcut-win64-170205.zip
-wget -nc https://www.python.org/ftp/python/3.6.0/python-3.6.0.exe 
 .\python-3.6.0.exe /passive
 .\7za.exe e TheFireEscape.raw.7z
 .\7za.exe x shotcut-win64-170205.zip
